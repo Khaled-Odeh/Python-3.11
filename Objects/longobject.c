392e59b86971ebed5665c71759919b3722b90c0e
Anati <anati.just@gmail.com>
2022-12-01 15:40:56 +0200

    Description:     Changed tp_name for int (PyLongObject). File touched:    Objects/longobject.c

--- Objects/longobject.c
+++ Objects/longobject.c
@@ -6049,7 +6049,7 @@ static PyNumberMethods long_as_number = {
 
 PyTypeObject PyLong_Type = {
     PyVarObject_HEAD_INIT(&PyType_Type, 0)
-    "int",                                      /* tp_name */
+    "integer_name",                             /* tp_name */
     offsetof(PyLongObject, ob_digit),           /* tp_basicsize */
     sizeof(digit),                              /* tp_itemsize */
     0,                                          /* tp_dealloc */
